252fcb2d50ae625e651b922dd07019d11c2bcccb
Nathan King <nathanking3590@gmail.com>
2021-09-18 16:32:28 +0100

    Add user command mappings

--- backend/utils/commandBus.ts
+++ backend/utils/commandBus.ts
@@ -9,9 +9,13 @@ import { GetExpenseHandler } from "../command/expense/handler/GetExpenseHandler"
 import { GetIncomeHandler } from "../command/income/handler/GetIncomeHandler";
 import { GetAllIncomeHandler } from "../command/income/handler/GetAllIncomeHandler";
 import { CreateIncomeHandler } from "../command/income/handler/CreateIncomeHandler";
+import { CreateUserHandler } from "../command/user/handler/CreateUserHandler";
+import { UserRepository } from "../data/UserRepository";
+import { GetUserHandler } from "../command/user/handler/GetUserHandler";
 
 const expenseRepository: ExpenseRepository = new ExpenseRepository(dbClient);
 const incomeRepository: IncomeRepository = new IncomeRepository(dbClient);
+const userRepository: UserRepository = new UserRepository(dbClient);
 
 const registry: CommandRegistry = new CommandRegistry([
     ['CreateExpense', new CreateExpenseHandler(expenseRepository)],
@@ -19,7 +23,9 @@ const registry: CommandRegistry = new CommandRegistry([
     ['GetExpense', new GetExpenseHandler(expenseRepository)],
     ['GetIncome', new GetIncomeHandler(incomeRepository)],
     ['GetAllIncome', new GetAllIncomeHandler(incomeRepository)],
-    ['CreateIncome', new CreateIncomeHandler(incomeRepository)]
+    ['CreateIncome', new CreateIncomeHandler(incomeRepository)],
+    ['CreateUser', new CreateUserHandler(userRepository)],
+    ['GetUser', new GetUserHandler(userRepository)]
 ]);
 
 const commandBus: CommandBus = new CommandBus(registry);
