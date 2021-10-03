db93c9aa2d00ca43ab49f56c44f8e46748900d3a
Nathan King <nathanking3590@gmail.com>
2021-10-03 22:01:34 +0100

    Add cors

--- backend/app.ts
+++ backend/app.ts
@@ -2,12 +2,15 @@ import { Server } from "./Server";
 import express, { Application } from 'express';
 import { Router } from "./Router";
 import commandBus from "./utils/commandBus";
-import bodyParser from "body-parser";
+import cors from 'cors';
 
 const app: Application = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const router: Router = new Router(app, '/api', commandBus);
 const server: Server = new Server(app, router);
